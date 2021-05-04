9c9089e11da73113d50e52f0ca20d658e5d21f80
milan2498 <milan24.nayak98@gmail.com>
2021-05-04 16:44:57 +0530

    course-server updtd

--- src/app/course-server.service.ts
+++ src/app/course-server.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseServerService {
 
+  baseUrl:string="http://localhost:4042/courses"
+
   constructor(private http:HttpClient) { }
+    getCourses(): Observable<any> {
+      return this.http.get(this.baseUrl);
+    }
+    deleteCourses(cid: number): Observable<any> {
+      //include responseType in options because response by default is JSON
+      return this.http.delete(`${this.baseUrl}/${cid}`, { responseType: 'text' })  // this.baseUrl+"/"+eid
+    }
+  
 }
