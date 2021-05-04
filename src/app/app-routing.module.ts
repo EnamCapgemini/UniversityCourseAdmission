b58dcd36fda5d0795039c1b88d3e471e695f1b88
EnamCapgemini <81413936+EnamCapgemini@users.noreply.github.com>
2021-05-04 16:21:54 +0530

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { ApplicantListComponent } from './applicant-list/applicant-list.componen
 import { ApplicantUpdateComponent } from './applicant-update/applicant-update.component';
 import { HomeComponent } from './home/home.component';
 
+import { LoginComponent } from './login/login.component'
+import { LogoutComponent } from './logout/logout.component'
+
 const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'aboutus',component:AboutusComponent},
   {path:'applicant-create',component:ApplicantCreateComponent},
   {path:'applicant-list',component:ApplicantListComponent},
-  {path:'applicant-update',component:ApplicantUpdateComponent}
-];
+  {path:'applicant-update',component:ApplicantUpdateComponent},
+  {path:'login', component:LoginComponent},
+  {path:'logout',component:LogoutComponent}
+];  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
