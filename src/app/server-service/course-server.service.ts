b101a609412427dd0b68c6e26521bdf2d76cf6e9
milan2498 <milan24.nayak98@gmail.com>
2021-05-05 10:58:31 +0530

    Update course-server.service.ts

--- src/app/server-service/course-server.service.ts
+++ src/app/server-service/course-server.service.ts
@@ -5,17 +5,25 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class CourseServerService {
+export class CourseServerService { 
 
-  baseUrl:string="http://localhost:4042/courses"
+  baseUrl:string="http://localhost:8082/Courses"
 
   constructor(private http:HttpClient) { }
     getCourses(): Observable<any> {
       return this.http.get(this.baseUrl);
     }
+    getCourse(cid: number):Observable<any> {
+      return this.http.get(`${this.baseUrl}/${cid}`)
+    }
+    updateCourse(cid: number):Observable<any> {
+      return this.http.put(`${this.baseUrl}/${cid}`, { responseType: 'text' });
+    }
     deleteCourses(cid: number): Observable<any> {
       //include responseType in options because response by default is JSON
       return this.http.delete(`${this.baseUrl}/${cid}`, { responseType: 'text' })  // this.baseUrl+"/"+eid
     }
-  
+    addCourses(sid: number) :Observable<any>{
+      return this.http.post(`${this.baseUrl}/${sid}`, { responseType: 'text' });
+    }
 }
