b847dd44d9d9590b725b48c73eb74e8bad018f92
ayush5691 <hackib020@gmail.com>
2021-05-06 13:37:05 +0530

    update

--- src/app/admission-update/admission-update.component.ts
+++ src/app/admission-update/admission-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Admission } from 'src/model/admission';
+import { AdmissionServerService } from '../server-service/admission-server.service';
 
 @Component({
   selector: 'app-admission-update',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdmissionUpdateComponent implements OnInit {
 
-  constructor() { }
+  admission:Admission=null;
+
+  validationMessages: string[] = null;
+  errorMessage: string = null;
+  successMessage: string = null;
+  admissionId: number;
+
+  constructor(private service:AdmissionServerService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(
+      (params) => {
+        let aid: number = +params.get('admissionId');
+
+        this.service.getAdmission(aid).subscribe(
+          (data) => {
+            this.admission = data
+          },
+          (fail) => {
+            this.errorMessage = fail.error.errorMessage;
+          }
+        )
+      }
+    )
   }
+
+  updated() {
+    this.service.updateAdmission(this.admission).subscribe(
+      (message) => {
+        this.successMessage=message
+        this.validationMessages = null
+        this.errorMessage = null
+      },
+      (failure) => {
+        this.successMessage = null;
+        this.validationMessages = JSON.parse(failure.error).errors;
+        this.errorMessage = JSON.parse(failure.error).errorMessage;
+      }
+
+    )
+
+  }
+
+  goBack(){
+    this.router.navigate(["admission-list"]);
+  }
+
 
 }
