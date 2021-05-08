282a95008fe425ece75a190122a915d3f5f5ed31
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-05-08 21:59:03 +0530

    buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title='Online University Management System'
+constructor(){}
+ngOnInit(){
+// Add active class to the current button (highlight it)
+let header = document.getElementById("myDIV");
+let btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  if (current.length > 0) { 
+    current[0].className = current[0].className.replace(" active", "");
+  }
+  this.className += " active";
+  });
+}
+}
 }
