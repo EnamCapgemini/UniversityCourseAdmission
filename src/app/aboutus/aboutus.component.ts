600848cb3f0a6d61de63e1e722bc2dea5b3ac134
dasp7601 <dasp7601@gmail.com>
2021-05-08 21:59:35 +0530

    Update aboutus.component.ts

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -14,12 +14,12 @@ export class AboutusComponent implements OnInit {
   }
 
   aboutUs:aboutus[]=[
-    {name:'Md Enam',role:'Team Leader',description:'Capgemini ID: 46059502',mailId:'enam@capgemini.com'},
-    {name:'Supratim',role:'Colleague1',description:'Capgemini ID: 46060201',mailId:'supratim@capgemini.com'},
-    {name:'Pritam',role:'Colleague2',description:'Capgemini ID: 46060215',mailId:'pritam@capgemini.com'},
-    {name:'Milan',role:'Colleague3',description:'Capgemini ID: 46059162',mailId:'milan@capgemini.com'},
-    {name:'Ayush',role:'Colleague4',description:'Capgemini ID: 46060216',mailId:'ayush@capgemini.com'},
-    {name:'Anirban',role:'Colleague5',description:'Capgemini ID: 46060210',mailId:'anirban@capgemini.com'}
+    {name:'Md Enam',role:'Principal',description:'Capgemini ID: 46059502',mailId:'enam@capgemini.com'},
+    {name:'Supratim',role:'Subject Expert',description:'Capgemini ID: 46060201',mailId:'supratim@capgemini.com'},
+    {name:'Pritam',role:'Manager',description:'Capgemini ID: 46060215',mailId:'pritam@capgemini.com'},
+    {name:'Milan',role:'Recruiter',description:'Capgemini ID: 46059162',mailId:'milan@capgemini.com'},
+    {name:'Ayush',role:'Accounts Head',description:'Capgemini ID: 46060216',mailId:'ayush@capgemini.com'},
+    {name:'Anirban',role:'Administrator',description:'Capgemini ID: 46060210',mailId:'anirban@capgemini.com'}
   ]
 
 }
