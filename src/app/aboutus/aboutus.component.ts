f28f1784a18689311c8fda7a182778f44f4fd948
SupratimSarkar19 <supratimsarkar99@gmail.com>
2021-05-14 20:28:13 +0530

    xxxx

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -14,12 +14,12 @@ export class AboutusComponent implements OnInit {
   }
 
   aboutUs:aboutus[]=[
-    {name:'Md Enam',role:'LogIn Module',description:'Capgemini ID: 46059502',mailId:'enam@capgemini.com'},
-    {name:'Supratim',role:'UniversityStaff Module',description:'Capgemini ID: 46060201',mailId:'supratim@capgemini.com'},
-    {name:'Pritam',role:'Applicant Module',description:'Capgemini ID: 46060215',mailId:'pritam@capgemini.com'},
-    {name:'Milan',role:'Course Module',description:'Capgemini ID: 46059162',mailId:'milan@capgemini.com'},
-    {name:'Ayush',role:'Admission Module',description:'Capgemini ID: 46060216',mailId:'ayush@capgemini.com'},
-    {name:'Anirban',role:'Admission Commitee Module',description:'Capgemini ID: 46060210',mailId:'anirban@capgemini.com'}
+    {name:'Md Enam',role:'Principal',description:'Capgemini ID: 46059502',mailId:'enam@capgemini.com'},
+    {name:'Supratim',role:'Subject Expert',description:'Capgemini ID: 46060201',mailId:'supratim@capgemini.com'},
+    {name:'Pritam',role:'Manager',description:'Capgemini ID: 46060215',mailId:'pritam@capgemini.com'},
+    {name:'Milan',role:'Recruiter',description:'Capgemini ID: 46059162',mailId:'milan@capgemini.com'},
+    {name:'Ayush',role:'Accounts Head',description:'Capgemini ID: 46060216',mailId:'ayush@capgemini.com'},
+    {name:'Anirban',role:'Administrator',description:'Capgemini ID: 46060210',mailId:'anirban@capgemini.com'}
   ]
 
 }
